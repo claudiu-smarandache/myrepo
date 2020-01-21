b33421cf3395fead85b62a025f9ca151cafa094e
claudiu-smarandache <60132041+claudiu-smarandache@users.noreply.github.com>
2020-01-21 12:58:28 +0200

    Create TestSuite2.java

---
+++ TestSuite2.java
@@ -0,0 +1,7 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CountryTests.class, BuildingTests.class})
+public class TestSuite2 {
+}
