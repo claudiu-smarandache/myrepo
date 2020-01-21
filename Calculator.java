da928f4f8d88adc04052e48d43f17838b6f2e15c
claudiu-smarandache <60132041+claudiu-smarandache@users.noreply.github.com>
2020-01-21 11:05:18 +0200

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+public class Exchange {
+    private Integer firstNumber;
+    private Integer secondNumber;
+
+    public Exchange() {
+    }
+
+    public Exchange(Integer firstNumber, Integer secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public String Multiplicate() {
+        return this.firstNumber * this.secondNumber;
+    }
+}
