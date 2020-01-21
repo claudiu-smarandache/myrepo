35f10f3c71d60dc064f0943c7683516ce1791dc2
claudiu-smarandache <60132041+claudiu-smarandache@users.noreply.github.com>
2020-01-21 12:57:37 +0200

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    private String name;
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Boolean isMajor(){
+        return this.age >= 18;
+    }
+}
