77aad397d6fddb6095b49524d490edc6767d9c5b
claudiu-smarandache <60132041+claudiu-smarandache@users.noreply.github.com>
2020-01-21 12:56:27 +0200

    Create Building.java

---
+++ Building.java
@@ -0,0 +1,14 @@
+public class Building {
+    private Integer noOfFloors;
+
+    public Building() {
+    }
+
+    public Building(Integer noOfFloors) {
+        this.noOfFloors = noOfFloors;
+    }
+
+    public Integer getNoOfFloors() {
+        return noOfFloors;
+    }
+}
